a78eaa5be94d6e52aea9d11a1d10f64d3bf5e90f
shavit shelli <shavit.shelli57@gmail.com>
2023-04-16 18:38:14 +0300

    secouunt commit

--- hello.java
+++ hello.java
@@ -6,7 +6,7 @@ public class hello
     {
         for(int i=1; i<=10; i++)
         {
-            System.out.println("hello world"+i);
+            System.out.println("hello my world"+i);
         }
     }
 }
